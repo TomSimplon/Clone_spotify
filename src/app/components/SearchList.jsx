810d7aefeb097427dcebb78ca78a049df5de9988
tom <tom.pustel.simplon@gmail.com>
2023-06-27 12:03:12 +0200

    barre de recherche dynamique

--- src/app/components/SearchList.jsx
+++ src/app/components/SearchList.jsx
@@ -5,32 +5,35 @@ import { useEffect, useState } from "react"
 import { fetchClientSpotifyApi } from "./api"
 import styles from '../search/page.module.scss'
 
-const SearchList = (token) => {
-    const [results, setResults] = useState();
+const SearchList = ({ token }) => {
+    const [results, setResults] = useState([]);
+
     const searchParams = useSearchParams()
-    const search = searchParams.get('query')
+    const search = searchParams.get('query');
 
     useEffect(() => {
-    const fetch = async () => {
-        const data = await fetchClientSpotifyApi(`search?q=${search}&type=album%2Cplaylist%2Cartist&market=fr&limit=25&offset=0`, token.token)
-        console.log(data)
-    }
-    fetch()
+        const fetchData = async () => {
+            const data = await fetchClientSpotifyApi(`search?q=${search}&type=album%2Cplaylist%2Cartist&market=fr&limit=25&offset=0`, token);
+            setResults(data.albums.items, data.artists.items, data.playlists.items);
+        };
 
-    }, [search])
-    console.log(results)
+        fetchData();
+    }, [search, token]);
+
+    console.log(results);
 
     return (
         <div className={styles.container}>
-      {/* {results.map((result) => (
-        <div key={result.id} className={styles.block}>
-          <Image src={result.images[0].url} width={200} height={200} className="miniature" alt="miniature"></Image>
-          <p className={styles.title}>{result.name}</p>
-          <p className={styles.p}>{result.artists[0].name}</p>
-          <p className={styles.date}>{new Date(result.release_date).toLocaleDateString()}</p>
+            { Array.isArray(results) &&
+            results.map((result) => (
+                <div key={result.id} className={styles.block}>
+                    <img src={result.images[0].url} alt="miniature" />
+                    <p className={styles.title}>{result.name}</p>
+                    <p className={styles.p}>{result.artists[0].name}</p>
+                    <p className={styles.date}>{new Date(result.release_date).toLocaleDateString()}</p>
+                </div>
+            ))}
         </div>
-      ))} */}
-      </div>
     )
 }
 
